d997c6d6549b682f460520260caca61f2f3d1356
orennn12 <satrios.playgame@gmail.com>
2025-06-24 19:38:04 +0800

    Struktur Dasar Program

---
+++ README.md
@@ -0,0 +1,8 @@
+# DeskripsiAdd
+Ini merupakan sebuah repository untuk memenuhi tugas yang diberikan oleh Pak Ade dalam mata kuliah basis data, Dalam hal ini kami membuat sebuah sistem inventaris yang berbasis linked list singly. Kami beranggotakan:
+
+1. [Hipni](https://github.com/Haippp) (C030324071)
+2. [Dzaki Ahmad Andreaz](https://github.com/Yellowdudee) (C030324115)
+3. [Rizky Satrio Wicaksono](https://github.com/orennn12) (C030324107)
+
+Lihat Selengkapnya [di sini](https://github.com/Haippp)
---
+++ invPercetakan.cpp
@@ -0,0 +1,31 @@
+#include <iostream>Add commentMore actions
+using namespace std;
+
+struct printing_item
+{
+    string nama;
+    string deskripsi;
+    int stok;
+    printing_item *selanjutnya;
+};
+
+void tambah_item(printing_item* head, string nm, string desk, int stok){
+}
+
+void sisipkan_item(printing_item* head, string nm, string desk, int stok){
+}
+
+void update_item(printing_item* head, string nm){
+}
+
+void hapus_item(printing_item* head, string nm){
+}
+
+void tampilkan_item(printing_item* head){
+}
+
+int main(){
+    cout << "hello world" << endl;
+
+    return 0;
+}
